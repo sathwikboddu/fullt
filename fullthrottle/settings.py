dab3e597826ca82edeedf28869b7ff4cc1efa12e
Sathwik <boddusathwik45@gmail.com>
2020-05-31 11:07:05 +0530

    updated

--- fullthrottle/settings.py
+++ fullthrottle/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'fullthrottle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-SECRET_KEY = config('dfjlskfjalskfjdlafsa')
+SECRET_KEY = 'dfjlskfjalskfjdlafsa'
 DEBUG = config('DEBUG', default=False, cast=bool)
 DATABASES = {
     'default': dj_database_url.config(
