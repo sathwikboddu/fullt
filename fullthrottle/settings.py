5ef32d4b6b86f8ba88bd145a8a58459c7a37727c
Sathwik <boddusathwik45@gmail.com>
2020-05-31 11:19:42 +0530

    updated

--- fullthrottle/settings.py
+++ fullthrottle/settings.py
@@ -109,7 +109,7 @@ try:
         if url.scheme == 'mysql':
             DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
 except Exception:
-    print 'Unexpected error:', sys.exc_info()
+    print('Unexpected error:', sys.exc_info())
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
